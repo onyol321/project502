6beabc972168da4c9e8b2c4cae679d92a71583ae
onyol321 <onyolrebatis@gmail.com>
2025-05-05 17:08:35 +0800

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const allowedOrigin = ["https://fb-sharer-by-bogart.vercel.app","https://lalat.vercel.app"];
+const allowedOrigin = ["https://fb-sharer-by-bogart.vercel.app","https://sharebooster-by-bogart.vercel.app"];
 
 app.use(cors());
 app.use(express.json());
